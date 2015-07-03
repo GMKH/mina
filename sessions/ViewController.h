760cf0d2d3824445deb2e0b43450d74d7658df05
khosravi <minak@Aslaks-MacBook-Pro.local>
2015-07-03 15:10:32 +0200

    Initial Commit

---
+++ sessions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sessions
+//
+//  Created by Mina Khosravifard on 03/07/15.
+//  Copyright (c) 2015 Mina Khosravifard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sessions/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sessions
+//
+//  Created by Mina Khosravifard on 03/07/15.
+//  Copyright (c) 2015 Mina Khosravifard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
